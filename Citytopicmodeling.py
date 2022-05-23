f86b2b82ba80a495209a56dd00dab42b550c341e
A-AJRAD <32738331+A-AJRAD@users.noreply.github.com>
2022-05-23 14:44:02 -0400

    Update Citytopicmodeling.py

--- Citytopicmodeling.py
+++ Citytopicmodeling.py
@@ -22,8 +22,8 @@ from nltk.stem import WordNetLemmatizer
 
 # In[31]:
 
+url="https://raw.githubusercontent.com/A-AJRAD/Citymodeling/main/Abha.csv"
 
-url="https://raw.githubusercontent.com/Ochie123/Citytopic-modeling/main/Riyadh.csv"
 my_dataset=pd.read_csv(url)
 ##s=requests.get(url).content
 ##my_dataset=pd.read_csv(io.StringIO(s.decode('utf-8')))
