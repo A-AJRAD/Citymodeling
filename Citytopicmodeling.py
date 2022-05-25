3b20f993e33c89cd3de8a154d60a2ef284d51599
A-AJRAD <32738331+A-AJRAD@users.noreply.github.com>
2022-05-25 23:23:43 +0300

    Update Citytopicmodeling.py

--- Citytopicmodeling.py
+++ Citytopicmodeling.py
@@ -22,7 +22,7 @@ from nltk.stem import WordNetLemmatizer
 
 # In[31]:
 
-url="https://raw.githubusercontent.com/A-AJRAD/Citymodeling/main/Najran.csv"
+url="https://raw.githubusercontent.com/A-AJRAD/Citymodeling/main/Khamis Mushait.csv"
 
     
 
